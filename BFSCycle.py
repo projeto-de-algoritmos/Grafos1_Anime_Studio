25cbf89be4203094bcbfe4944650901fce4c97af
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2021-02-12 12:51:47 -0300

    Added graph print.

--- BFSCycle.py
+++ BFSCycle.py
@@ -4,20 +4,25 @@ from typing import DefaultDict
 class Node:
 	def __init__(self, number: int):
 			self.number = number
-			self.Visited = False
+			self.Visited = True
 			self.Elos = {}
 	
 	def addElo(self, nextNode):
 		self.Elos[len(self.Elos)] = (Elo(nextNode))
 
 	def printElos(self):
+		print(f"{self.number}-------->", end="")
 		for i in self.Elos:
-			print(self.Elos[i].childNode.number)
+			if i == len(self.Elos) - 1:
+				print(f"{self.Elos[i].childNode.number}")
+			else:
+				print(f"{self.Elos[i].childNode.number}-------->", end="")
+		print("")
 
 class Elo:
 	def __init__(self, childNode: Node):
 			self.childNode = childNode
-			self.Visited = False
+			self.Visited = 0
 
 
 class Grafo:
@@ -27,37 +32,64 @@ class Grafo:
 	def addNode(self, node: Node):
 			self.Nodes[node.number] = node
 
-	def addElo(self, currentNode: Node, nextNode: Node):
+	def addElo(self, currentNode: Node, nextNode: Node): # This is a direcioned Elo, possible fix: make the 2 nodes share the same elo 
+
 		if currentNode.number in self.Nodes:
 			currentNode = self.Nodes.get(currentNode.number)
 		else:
 			self.addNode(currentNode)
-		
+
 		if nextNode.number in self.Nodes:
 			nextNode = self.Nodes.get(nextNode.number)
 		else:
 			self.addNode(nextNode)
 		
 		self.Nodes[currentNode.number].addElo(nextNode)
-
-		
+		# self.Nodes[nextNode.number].addElo(currentNode)
+	def printGraph(self):
+		for i in self.Nodes:
+			if self.Nodes[i].Elos:
+				self.Nodes[i].printElos()
+	
+	def BFS(self, root: int):
+		pass
 
 
 
 
 g = Grafo()
 
+g.addElo(Node(1), Node(2))
+g.addElo(Node(1), Node(3))
 
-n1 = Node(1)
+g.addElo(Node(2), Node(1))
+g.addElo(Node(2), Node(3))
+g.addElo(Node(2), Node(4))
+g.addElo(Node(2), Node(5))
 
-n1.addElo(Node(0))
+g.addElo(Node(3), Node(1))
+g.addElo(Node(3), Node(2))
+g.addElo(Node(3), Node(5))
+g.addElo(Node(3), Node(7))
+g.addElo(Node(3), Node(8))
 
-n1.printElos()
+g.addElo(Node(4), Node(2))
+g.addElo(Node(4), Node(5))
 
-print("Recreating")
+g.addElo(Node(5), Node(3))
+g.addElo(Node(5), Node(2))
+g.addElo(Node(5), Node(4))
+g.addElo(Node(5), Node(6))
 
-n1.addElo(Node(1))
+g.addElo(Node(6), Node(5))
 
-n1.printElos()
+g.addElo(Node(7), Node(3))
+g.addElo(Node(7), Node(8))
 
+g.addElo(Node(8), Node(3))
+g.addElo(Node(8), Node(7))
+
+g.addElo(Node(9), Node(10))
+g.addElo(Node(10), Node(9))
+g.printGraph()
 # g.addElo2Nodes(g.Nodes[0], g.Nodes[1])
